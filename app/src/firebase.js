7f1c8a6fab96cbff5dd9fa00b0e95478d9d2b0fa
GizemOnur <gizem.onur0425@gmail.com>
2022-01-03 19:05:30 +0100

    first try with MUI

---
+++ app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJkvpKibuVRO37QFLTyUVioHjIRMZAq9Y",
+    authDomain: "bookface-9e131.firebaseapp.com",
+    projectId: "bookface-9e131",
+    storageBucket: "bookface-9e131.appspot.com",
+    messagingSenderId: "360549186376",
+    appId: "1:360549186376:web:6586958e05590be166d17d",
+    measurementId: "G-ZS26ENV5GT"
+  };
