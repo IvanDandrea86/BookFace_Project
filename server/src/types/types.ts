cc4917e42fce62b1bed243d109d852b5f654a5d4
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-01-06 11:08:50 +0100

    user resolver complete

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,5 +1,6 @@
 import { prop } from "@typegoose/typegoose";
 import { Field, ObjectType,} from "type-graphql";
+import {User} from '../entities/user.entity'
 
 @ObjectType()
 export class LikeState {
@@ -30,6 +31,32 @@ export class FriendRequestStatus {
     @Field({ defaultValue: false })
     @prop()
     refused :boolean;
+}
+
+@ObjectType()
+export class FieldError {
+  @Field(() => String)
+  field: string;
+  @Field(() => String)
+  message: string;
+  constructor(field:string,message:string){
+    this.field=field;
+    this.message=message;
+  }
+}
+@ObjectType()
+export class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+@ObjectType()
+export class FriendResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 
