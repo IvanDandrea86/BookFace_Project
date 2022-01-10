f86df24916ced175b071876bb2dca97a8c35b822
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-01-10 19:15:17 +0100

    update signup login

--- server/src/seeder/post.ts
+++ server/src/seeder/post.ts
@@ -9,7 +9,7 @@ export const seedMongoWithPosts=async(howmuch:number)=>{
    
     for(let i=0; i<howmuch;i+=1){
         const _id = new ObjectId();
-        const content = faker.lorem.paragraph(10)
+        const content =  faker.lorem.paragraph(10)
         const randomUser = await UserModel.aggregate([{ $sample: { size: 1 } }])
     
         let post = new PostModel({
