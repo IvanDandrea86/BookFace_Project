dc2bfb73904b9640724c4a1e561858dd5a0b49da
ClaraCliment <climent.clara@gmail.com>
2022-01-14 17:49:58 +0100

    Stories changing ... not finished yet

--- app/src/Component/StoryReel.js
+++ app/src/Component/StoryReel.js
@@ -4,27 +4,29 @@ import Grid from '@mui/material/Grid';
 
 function StoryReel() {
 
+    const image= "https://picsum.photos/id/" + Math.floor(Math.random()*1000) + "/800/600/"
+
     return (
 
         <Grid container className='storyReel'item Grid sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexWrap: "wrap"}}>
             <Story item
-            profileSrc="https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/93305513_10222305703541686_8570343274772955136_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=wFkrtIqhl64AX93_Sgk&_nc_ht=scontent.fbru1-1.fna&oh=00_AT_ipbk0Wc26bdv340MKC9fMG6bQQCcasFUkrPPU7_RnGg&oe=61FB46BA"
-            image="https://images.unsplash.com/photo-1633329128338-42096105a4a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80"
-            title="User Name"
+            profileSrc={image}
+            image={image}
+            title="Gizem Onur"
         />
         <Story item
-            profileSrc="https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/93305513_10222305703541686_8570343274772955136_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=wFkrtIqhl64AX93_Sgk&_nc_ht=scontent.fbru1-1.fna&oh=00_AT_ipbk0Wc26bdv340MKC9fMG6bQQCcasFUkrPPU7_RnGg&oe=61FB46BA"
-            image="https://images.unsplash.com/photo-1633329128338-42096105a4a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80"
+            profileSrc={image}
+            image={image}
             title="Clara Climent"
         />
         <Story itel
-            profileSrc="https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/93305513_10222305703541686_8570343274772955136_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=wFkrtIqhl64AX93_Sgk&_nc_ht=scontent.fbru1-1.fna&oh=00_AT_ipbk0Wc26bdv340MKC9fMG6bQQCcasFUkrPPU7_RnGg&oe=61FB46BA"
-            image="https://images.unsplash.com/photo-1633329128338-42096105a4a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80"
+            profileSrc={image}
+            image={image}
             title="Samir Nacer"
         />
         <Story
-            profileSrc="https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/93305513_10222305703541686_8570343274772955136_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=wFkrtIqhl64AX93_Sgk&_nc_ht=scontent.fbru1-1.fna&oh=00_AT_ipbk0Wc26bdv340MKC9fMG6bQQCcasFUkrPPU7_RnGg&oe=61FB46BA"
-            image="https://images.unsplash.com/photo-1633329128338-42096105a4a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80"
+            profileSrc={image}
+            image={image}
             title="Ivan Dandrea"
         />
       
