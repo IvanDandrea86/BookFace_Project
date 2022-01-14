6a188a7f132463883d4870ecd82e999f78d210b6
ClaraCliment <climent.clara@gmail.com>
2022-01-14 19:15:11 +0100

    Dates in MyPost.js

--- app/src/App.js
+++ app/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
     <Router>
 
        
-      <div className="App" >
+      <div className="App" style={{backgroundColor: "#fcfcfc"}}>
         <Switch>
           {!context.auth ? (
             <Route exact path="/">
--- app/src/Component/StoryReel.js
+++ app/src/Component/StoryReel.js
@@ -4,29 +4,29 @@ import Grid from '@mui/material/Grid';
 
 function StoryReel() {
 
-    const image= "https://picsum.photos/id/" + Math.floor(Math.random()*1000) + "/800/600/"
+    
 
     return (
 
         <Grid container className='storyReel'item Grid sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexWrap: "wrap"}}>
             <Story item
-            profileSrc={image}
-            image={image}
+            profileSrc="https://picsum.photos/id/1047/800/600/"
+            image="https://picsum.photos/id/1015/800/600/"
             title="Gizem Onur"
         />
         <Story item
-            profileSrc={image}
-            image={image}
+            profileSrc="https://picsum.photos/id/1045/800/600/"
+            image="https://picsum.photos/id/1002/800/600/"
             title="Clara Climent"
         />
         <Story itel
-            profileSrc={image}
-            image={image}
+            profileSrc="https://picsum.photos/id/1049/800/600/"
+            image="https://picsum.photos/id/1035/800/600/"
             title="Samir Nacer"
         />
         <Story
-            profileSrc={image}
-            image={image}
+            profileSrc="https://picsum.photos/id/1061/800/600/"
+            image="https://picsum.photos/id/1033/800/600/"
             title="Ivan Dandrea"
         />
       
