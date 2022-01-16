16ad25d32f29c284e910cf5d9ee085cd9e37972e
Ivan <ivan.d.andrea1@gmail.com>
2022-01-16 19:18:12 +0100

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -49,12 +49,12 @@ let nStartTime = Date.now()
   );
   
     
-    app.use(express.static(path.join(__dirname, '../../build/')));
+  app.use(express.static(path.resolve(__dirname, '../../app/build')));
   app.get('*', ( req:Request,res:Response)=>{
-    res.sendFile('index.html', {root: path.join(__dirname, '../../build/')});
+    res.sendFile('index.html', {root: path.join(__dirname, '../../app/build/')});
   });
   app.get('/favicon.ico', ( req:Request,res:Response)=>{
-    res.sendFile('favicon.ico', {root: path.join(__dirname, '../../build/')});
+    res.sendFile('favicon.ico', {root: path.join(__dirname, '../../app/build/')});
   });
   
 
