79ce281d7d6a3ac90324be34f6ba67b310b0644b
Ivan <ivan.d.andrea1@gmail.com>
2022-01-16 19:25:34 +0100

    update

--- app/src/Util/createApolloClient.js
+++ app/src/Util/createApolloClient.js
@@ -3,7 +3,7 @@ import {
     InMemoryCache,
   } from "@apollo/client";
   export const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: 'http://3ac8yvzvmllpvh:5vu79g37gj9buxmnaz6atkwfq9nwj@eu-west-static-05.quotaguard.com:9293/graphql',
     cache: new InMemoryCache(),
     credentials:"include",
   });
