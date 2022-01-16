0b8e17a9062ac997011ce7abcf55ef8a2636837d
Ivan <ivan.d.andrea1@gmail.com>
2022-01-16 15:50:13 +0100

    herko

--- server/src/server.ts
+++ server/src/server.ts
@@ -61,7 +61,7 @@ let nStartTime = Date.now()
     console.error(err);
   });
 
-  app.listen(PORT, () => {
+  app.listen( process.env.PORT || PORT, () => {
   console.log(startTime,`\n🚀 Server running at: http://localhost:${PORT}`);
   });
 
